56d5177ca97f217e3a9232d674335f2264f1b890
Rhys Ulerich <rhys.ulerich@gmail.com>
2019-10-05 09:06:28 -0400

    Update shebang for positional

--- basic/quicktest.c
+++ basic/quicktest.c
@@ -1,5 +1,5 @@
 #if 0
-exec ../c99sh -t 'test()' "$0"
+exec ../c99sh -t 'test()' "$0" "$@"
 #endif
 // Dual shebang allowing valid C source to be quickly tested like a script
 // (in scripts skip the ../ prefix).  That is, both ./quicktest.c and
